c5989d35ecf96bb3d1809b8cb78f9e4161593e91
Peter <pveijk@gmail.com>
2016-02-11 19:16:19 +0000

    ec2 instances

--- aws.py
+++ aws.py
@@ -4,6 +4,9 @@ from __future__ import print_function
 # By Peter HJ van Eijk 
 
 # uses env variables for AWS authentication.
+# sudo easy_install awscli
+# command line: aws configure
+# aws ec2 describe-availability-zones
 
 import json
 import urllib
@@ -14,7 +17,17 @@ print('Loading function')
 ec2 = boto3.client('ec2')
 
 print(ec2.describe_availability_zones())
+print(ec2.describe_regions())
+regions = ec2.describe_regions().get('Regions',[] )
 
+for region in regions:
+    reg=region['RegionName']
+    print(reg)
+    ec2con = boto3.client('ec2',region_name=reg)        
+    print(ec2con.describe_instances()['Reservations'])
+    
+
+print("S3 buckets...")
 s3 = boto3.client('s3')
 s3 = boto3.resource('s3')
 
