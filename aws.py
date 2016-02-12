1aa87a63a4d21edce739b3ca3abc436d31175635
Peter <pveijk@gmail.com>
2016-02-12 10:56:03 +0000

    nicer layout

--- aws.py
+++ aws.py
@@ -5,34 +5,39 @@ from __future__ import print_function
 
 # uses env variables for AWS authentication.
 # sudo easy_install awscli
-# command line: aws configure
+# command line version: aws configure
 # aws ec2 describe-availability-zones
 
 import json
 import urllib
 import boto3
 
-print('Loading function')
+def times2(x):
+    return(x * x) 
+
+print(times2(2))
 
 ec2 = boto3.client('ec2')
 
-print(ec2.describe_availability_zones())
-print(ec2.describe_regions())
+print("Regions", end=" ")
 regions = ec2.describe_regions().get('Regions',[] )
+print(regions)
+print("Zones", end=" "),
+print(ec2.describe_availability_zones()['AvailabilityZones'])
 
 for region in regions:
     reg=region['RegionName']
-    print(reg)
+    print(reg, end=" ")
     ec2con = boto3.client('ec2',region_name=reg)        
     print(ec2con.describe_instances()['Reservations'])
-    
 
 print("S3 buckets...")
 s3 = boto3.client('s3')
 s3 = boto3.resource('s3')
 
 for bucket in s3.buckets.all():
-    print(bucket.name)
+    print(bucket.name, end=" ")
+    
+print("")    
 
 # more ideas. Pull status info from other API endpoints. I.e. IOT devices. DNS providers. 
-
